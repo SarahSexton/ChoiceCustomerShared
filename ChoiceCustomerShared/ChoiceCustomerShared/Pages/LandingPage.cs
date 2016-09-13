bfd69fb5c1b5ac4bc020ea611bf6549607a01159
Sarah Sexton <sarahjeannesexton@live.com>
2016-09-13 15:16:55 -0700

    Demo code, Xamarin front-end
    Xamarin shared front-end project to illustrate example of Master Detail
    Pages

---
+++ ChoiceCustomerShared/ChoiceCustomerShared/Pages/LandingPage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace ChoiceCustomerShared.Pages
+{
+    public class LandingPage : MasterDetailPage
+    {
+        public LandingPage()
+        {
+            this.Master = new ContentPage { BackgroundColor = Color.Red, Title = "My Master" };
+            this.Detail = new NavigationPage(new ContentPage { BackgroundColor = Color.Blue, Title = "My Detail" });
+        }
+    }
+}
---
+++ ChoiceCustomerShared/ChoiceCustomerShared/Pages/Page2.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace ChoiceCustomerShared.Pages
+{
+	public partial class Page2 : ContentView
+	{
+		public Page2 ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
